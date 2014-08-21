dc0121c4cfd8b36c46e39712701521dc0cc41536
Jinzhu <wosmvp@gmail.com>
2014-08-21 10:09:03 +0800

    Include error in finder

--- resource/processor.go
+++ resource/processor.go
@@ -119,7 +119,7 @@ func (processor *processor) Commit() (errors []error) {
 }
 
 func (processor *processor) Start() (errors []error) {
-	processor.Initialize()
+	errors = []error{processor.Initialize()}
 	if errors = append(errors, processor.Validate()...); len(errors) == 0 {
 		errors = append(errors, processor.Commit()...)
 	}
