47ec6dfe2f621322b1615dd4231d5dab3abbc9f7
bom_d_van <bom.d.van@gmail.com>
2014-08-22 09:59:59 +0800

    exchange: support csv file

--- exchange/csvfile.go
+++ exchange/csvfile.go
@@ -1,17 +1,25 @@
 package exchange
 
+import (
+	"encoding/csv"
+
+	"io"
+)
+
 type CSVFile struct {
+	lines [][]string
 }
 
-func NewCSVFile() *CSVFile {
-	f := new(CSVFile)
-	return f
-}
-
-func (x *CSVFile) TotalLines() (num int) {
+func NewCSVFile(r io.Reader) (f *CSVFile, err error) {
+	f = new(CSVFile)
+	f.lines, err = csv.NewReader(r).ReadAll()
 	return
 }
 
-func (x *CSVFile) Line(l int) (fields []string) {
-	return
+func (f *CSVFile) TotalLines() (num int) {
+	return len(f.lines)
+}
+
+func (f *CSVFile) Line(l int) []string {
+	return f[l]
 }
