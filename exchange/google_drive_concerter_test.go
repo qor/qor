0d1a6d13f20529109ed75650d46d0a41d2461a92
bom_d_van <bom.d.van@gmail.com>
2014-08-26 15:12:05 +0800

    exchange: add google_drive_converter

---
+++ exchange/google_drive_concerter_test.go
@@ -0,0 +1,61 @@
+package exchange
+
+import (
+	"flag"
+	"io"
+	"os"
+
+	"testing"
+)
+
+var googleAPIEmail, googleAPIKeyFile, excelfile, csvfile string
+
+func init() {
+	flag.StringVar(&googleAPIEmail, "googleAPIEmail", "", "")
+	flag.StringVar(&googleAPIKeyFile, "googleAPIKeyFile", "", "")
+	flag.StringVar(&excelfile, "excelfile", "", "")
+	flag.StringVar(&csvfile, "csvfile", "", "")
+}
+
+func TestNewGoogleDriveCSVConverter(t *testing.T) {
+	flag.Parse()
+
+	if googleAPIEmail == "" || googleAPIKeyFile == "" {
+		return
+	}
+
+	converter, err := NewGoogleDriveConverter(googleAPIEmail, googleAPIKeyFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if excelfile != "" {
+		csvsrc, err := converter.Convert(excelfile, ExcelContentType, CSVContentType)
+		if err != nil {
+			t.Fatal("Can't convert excel to csv: %s", err)
+		}
+		csvdst, err := os.OpenFile("test.csv", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = io.Copy(csvdst, csvsrc)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	if csvfile != "" {
+		excelsrc, err := converter.Convert(csvfile, CSVContentType, ExcelContentType)
+		if err != nil {
+			t.Fatal("Can't convert csv to excel: %s", err)
+		}
+		exceldst, err := os.OpenFile("test.xlsx", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = io.Copy(exceldst, excelsrc)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
---
+++ exchange/google_drive_converter.go
@@ -0,0 +1,92 @@
+package exchange
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"code.google.com/p/google-api-go-client/drive/v2"
+	"github.com/golang/oauth2"
+	"github.com/golang/oauth2/google"
+)
+
+const (
+	ExcelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+	CSVContentType   = "text/csv"
+)
+
+type googleDriveFile struct {
+	*os.File
+	size        int
+	contentType string
+}
+
+func (f googleDriveFile) Len() int {
+	return f.size
+}
+
+func (f googleDriveFile) ContentType() string {
+	return f.contentType
+}
+
+type GoogleDriveConverter struct {
+	config *oauth2.JWTConfig
+}
+
+func NewGoogleDriveConverter(clientEmail, keyFilePath string) (gdc *GoogleDriveConverter, err error) {
+	key, err := ioutil.ReadFile(keyFilePath)
+	if err != nil {
+		return
+	}
+	gdc = new(GoogleDriveConverter)
+	gdc.config, err = google.NewServiceAccountConfig(&oauth2.JWTOptions{
+		Email:      clientEmail,
+		PrivateKey: key,
+		Scopes:     []string{"https://www.googleapis.com/auth/drive"},
+	})
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (gdc *GoogleDriveConverter) Convert(path, from, to string) (r io.Reader, err error) {
+	gdf := &googleDriveFile{contentType: from}
+	gdf.File, err = os.Open(path)
+	if err != nil {
+		return
+	}
+	stat, err := gdf.File.Stat()
+	if err != nil {
+		return
+	}
+	gdf.size = int(stat.Size())
+
+	client := http.Client{Transport: gdc.config.NewTransport()}
+	svc, err := drive.New(&client)
+	if err != nil {
+		return
+	}
+	scheme := &drive.File{MimeType: from}
+	file, err := svc.Files.Insert(scheme).Media(gdf).Convert(true).Do()
+	if err != nil {
+		return
+	}
+
+	csvLink, ok := file.ExportLinks[to]
+	if !ok {
+		err = fmt.Errorf("can't find ExportLinks for %s", to)
+		return
+	}
+
+	resp, err := client.Get(csvLink)
+	if err != nil {
+		return
+	}
+	r = resp.Body
+
+	return
+}
