8048ebe818611ca035195b71bc08254e91754c07
Jinzhu <wosmvp@gmail.com>
2014-09-03 11:33:14 +0800

    Tweak resolver

--- publish/resolver.go
+++ publish/resolver.go
@@ -13,22 +13,42 @@ type Resolver struct {
 }
 
 type Dependency struct {
-	Type   reflect.Type
-	Values []interface{}
+	Type        reflect.Type
+	PrimaryKeys []string
+}
+
+func IncludeValue(value string, values []string) bool {
+	for _, v := range values {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
 
 func (resolver *Resolver) AddDependency(dependency *Dependency) {
-	Dependencies
+	name := dependency.Type.String()
+	var primaryKeys []string
+
+	if dep, ok := resolver.Dependencies[name]; ok {
+		for _, primaryKey := range dependency.PrimaryKeys {
+			if !IncludeValue(value, dep.PrimaryKeys) {
+				dep.PrimaryKeys = append(dep.PrimaryKeys, primaryKey)
+			}
+		}
+	} else {
+		resolver.Dependencies[name] = dependency
+	}
 }
 
-func (resolver *Resolver) GetDependencies(dependency *Dependency) {
+func (resolver *Resolver) GetDependencies(dependency *Dependency, primaryKeys []string) {
 }
 
 func (resolver *Resolver) Publish() {
 	for _, record := range resolver.Records {
 		reflectValue := reflect.ValueOf(record)
 		scope := &gorm.Scope{Value: record}
-		dependency = Dependency{Type: reflectValue.Type(), Value: []interface{}{scope.PrimaryKey()}}
+		dependency := Dependency{Type: reflectValue.Type(), PrimaryKeys: []string{scope.PrimaryKey()}}
 		resolver.AddDependency(dependency)
 	}
 
