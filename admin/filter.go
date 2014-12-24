d6c3ebe1a24211f0a97ec2979c93191a1bad03d9
Jinzhu <wosmvp@gmail.com>
2014-12-24 22:44:27 +0800

    Add DefaultHandler

--- admin/filter.go
+++ admin/filter.go
@@ -1,6 +1,9 @@
 package admin
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/jinzhu/gorm"
 	"github.com/qor/qor"
 )
@@ -12,5 +15,23 @@ type Filter struct {
 }
 
 var DefaultHandler = func(name string, value string, scope *gorm.DB, context *qor.Context) *gorm.DB {
+	lastIndex := strings.LastIndex(name, "_")
+	operation := name[lastIndex+1 : len(name)]
+	column := name[0:lastIndex]
+
+	switch operation {
+	case "cont":
+		return scope.Where(fmt.Sprintf("%v ILIKE ?", scope.NewScope(nil).Quote(column)), "%"+value+"%")
+	case "eq":
+		return scope.Where(fmt.Sprintf("%v = ?", scope.NewScope(nil).Quote(column)), value)
+	case "gt":
+		return scope.Where(fmt.Sprintf("%v > ?", scope.NewScope(nil).Quote(column)), value)
+	case "gteq":
+		return scope.Where(fmt.Sprintf("%v >= ?", scope.NewScope(nil).Quote(column)), value)
+	case "lt":
+		return scope.Where(fmt.Sprintf("%v < ?", scope.NewScope(nil).Quote(column)), value)
+	case "lteq":
+		return scope.Where(fmt.Sprintf("%v <= ?", scope.NewScope(nil).Quote(column)), value)
+	}
 	return scope
 }
