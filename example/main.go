c581b7a87cce83023372e6245d99ca0544f07929
bom_d_van <bom.d.van@gmail.com>
2014-12-26 10:25:34 +0800

    worker: add examples in example/main.go

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/qor/qor/worker"
 
@@ -22,10 +23,10 @@ func main() {
 	user := admin.NewResource(User{})
 	user.IndexAttrs("fullname", "gender")
 	user.Meta(&resource.Meta{Name: "CreditCard", Resource: creditCard})
-
 	user.Meta(&resource.Meta{Name: "fullname", Alias: "name"})
 	user.Meta(&resource.Meta{Name: "gender", Type: "select_one", Collection: []string{"M", "F", "U"}})
-	user.Meta(&resource.Meta{Name: "RoleId", Label: "Role", Type: "select_one",
+	user.Meta(&resource.Meta{
+		Name: "RoleId", Label: "Role", Type: "select_one",
 		Collection: func(resource interface{}, context *qor.Context) (results [][]string) {
 			if roles := []Role{}; !context.GetDB().Find(&roles).RecordNotFound() {
 				for _, role := range roles {
@@ -33,8 +34,10 @@ func main() {
 				}
 			}
 			return
-		}})
-	user.Meta(&resource.Meta{Name: "Languages", Type: "select_many",
+		},
+	})
+	user.Meta(&resource.Meta{
+		Name: "Languages", Type: "select_many",
 		Collection: func(resource interface{}, context *qor.Context) (results [][]string) {
 			if languages := []Language{}; !context.GetDB().Find(&languages).RecordNotFound() {
 				for _, language := range languages {
@@ -42,7 +45,8 @@ func main() {
 				}
 			}
 			return
-		}})
+		},
+	})
 
 	config := qor.Config{DB: &db}
 	web := admin.New(&config)
@@ -51,12 +55,38 @@ func main() {
 	web.NewResource(Language{})
 	web.MountTo("/admin", mux)
 
-	w := worker.New("Log every 10 seconds")
-
-	worker.Listen()
-
-	// go worker
+	initWorkers()
 
 	fmt.Println("listening on :8080")
 	http.ListenAndServe(":8080", mux)
+}
+
+func initWorkers() {
+	if err := worker.SetJobDB(&db); err != nil {
+		panic(err)
+	}
+
+	bq := worker.NewBeanstalkdQueue("beanstalkd", "localhost:11300")
+	var counter int
+	w := worker.NewWorker(bq, "Log every 10 seconds", func(job *worker.Job) (err error) {
+		counter++
+		log, err := job.GetLogger()
+		if err != nil {
+			return
+		}
+
+		_, err = log.Write([]byte(strconv.Itoa(counter) + "\n"))
+
+		time.Sleep(time.Minute * 5)
+
+		return
+	})
+
+	// must be executed before http.ListenAndServer
+	worker.Listen()
+
+	job, err := w.NewJob(1, time.Now())
+	if err != nil {
+		panic(err)
+	}
 }
