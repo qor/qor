9e36990069873f72f6fb34619e16ee276b779a83
Jinzhu <wosmvp@gmail.com>
2014-12-30 13:14:25 +0800

    Replace Content with Context

--- admin/context.go
+++ admin/context.go
@@ -7,10 +7,14 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"reflect"
+	"strings"
 	"text/template"
 
+	"github.com/jinzhu/gorm"
 	"github.com/qor/qor"
 	"github.com/qor/qor/resource"
+	"github.com/qor/qor/roles"
 )
 
 type Context struct {
@@ -99,6 +103,132 @@ func (context *Context) Execute(name string, result interface{}) {
 // Function Maps
 func (context *Context) ValueOf(value interface{}, meta *resource.Meta) interface{} {
 	return meta.Value(value, context.Context)
+}
+
+func (context *Context) NewResourcePath(value interface{}) string {
+	if res, ok := value.(*Resource); ok {
+		return path.Join(context.Admin.router.Prefix, res.Name, "new")
+	} else {
+		return path.Join(context.Admin.router.Prefix, context.Resource.Name, "new")
+	}
+}
+
+func (context *Context) UrlFor(value interface{}, resources ...*Resource) string {
+	var url string
+	if admin, ok := value.(*Admin); ok {
+		url = admin.router.Prefix
+	} else if resource, ok := value.(*Resource); ok {
+		url = path.Join(context.Admin.router.Prefix, resource.Name)
+	} else {
+		primaryKey := context.GetDB().NewScope(value).PrimaryKeyValue()
+		name := strings.ToLower(reflect.Indirect(reflect.ValueOf(value)).Type().Name())
+		url = path.Join(context.Admin.router.Prefix, name, fmt.Sprintf("%v", primaryKey))
+	}
+	return url
+}
+
+func (context *Context) LinkTo(text interface{}, link interface{}) string {
+	if linkStr, ok := link.(string); ok {
+		return fmt.Sprintf(`<a href="%v">%v</a>`, linkStr, text)
+	}
+	return fmt.Sprintf(`<a href="%v">%v</a>`, context.UrlFor(link), text)
+}
+
+func (context *Context) RenderForm(value interface{}, metas []*resource.Meta) string {
+	var result = bytes.NewBufferString("")
+	context.renderForm(result, value, metas, []string{"QorResource"})
+	return result.String()
+}
+
+func (context *Context) renderForm(result *bytes.Buffer, value interface{}, metas []*resource.Meta, prefix []string) {
+	for _, meta := range metas {
+		context.RenderMeta(result, meta, value, prefix)
+	}
+}
+
+func (context *Context) RenderMeta(writer *bytes.Buffer, meta *resource.Meta, value interface{}, prefix []string) {
+	prefix = append(prefix, meta.Name)
+
+	funcsMap := context.funcMap(roles.Read, roles.Update)
+	funcsMap["render_form"] = func(value interface{}, metas []*resource.Meta, index ...int) string {
+		var result = bytes.NewBufferString("")
+		newPrefix := append([]string{}, prefix...)
+
+		if len(index) > 0 {
+			last := newPrefix[len(newPrefix)-1]
+			newPrefix = append(newPrefix[:len(newPrefix)-1], fmt.Sprintf("%v[%v]", last, index[0]))
+		}
+
+		context.renderForm(result, value, metas, newPrefix)
+		return result.String()
+	}
+
+	var tmpl = template.New(meta.Type + ".tmpl").Funcs(funcsMap)
+
+	if tmpl, err := context.findTemplate(tmpl, fmt.Sprintf("forms/%v.tmpl", meta.Type)); err == nil {
+		data := map[string]interface{}{}
+		data["InputId"] = strings.Join(prefix, "")
+		data["Label"] = meta.Label
+		data["InputName"] = strings.Join(prefix, ".")
+		data["Value"] = meta.Value(value, context.Context)
+		if meta.GetCollection != nil {
+			data["CollectionValue"] = meta.GetCollection(value, context.Context)
+		}
+		data["Meta"] = meta
+
+		if err := tmpl.Execute(writer, data); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Printf("%v: form type %v not supported\n", meta.Name, meta.Type)
+	}
+}
+
+func (context *Context) HasPrimaryKey(value interface{}, primaryKey interface{}) bool {
+	primaryKeys := []interface{}{}
+	reflectValue := reflect.ValueOf(value)
+	if reflectValue.Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+	}
+	if reflectValue.Kind() == reflect.Slice {
+		for i := 0; i < reflectValue.Len(); i++ {
+			scope := &gorm.Scope{Value: reflectValue.Index(i).Interface()}
+			primaryKeys = append(primaryKeys, scope.PrimaryKeyValue())
+		}
+	} else if reflectValue.Kind() == reflect.Struct {
+		scope := &gorm.Scope{Value: value}
+		primaryKeys = append(primaryKeys, scope.PrimaryKeyValue())
+	}
+
+	for _, key := range primaryKeys {
+		if fmt.Sprintf("%v", primaryKey) == fmt.Sprintf("%v", key) {
+			return true
+		}
+	}
+	return false
+}
+
+func (context *Context) AllowedMetas(modes ...roles.PermissionMode) func(reses ...*Resource) []*resource.Meta {
+	return func(reses ...*Resource) []*resource.Meta {
+		var res = context.Resource
+		if len(reses) > 0 {
+			res = reses[0]
+		}
+
+		return res.AllowedMetas(res.IndexMetas(), context, modes...)
+	}
+}
+
+func (context *Context) funcMap(modes ...roles.PermissionMode) template.FuncMap {
+	return template.FuncMap{
+		"allowed_metas":     context.AllowedMetas(modes...),
+		"value_of":          context.ValueOf,
+		"url_for":           context.UrlFor,
+		"new_resource_path": context.NewResourcePath,
+		"link_to":           context.LinkTo,
+		"render_form":       context.RenderForm,
+		"has_primary_key":   context.HasPrimaryKey,
+	}
 }
 
 // context.NewSearcher().FindAll
