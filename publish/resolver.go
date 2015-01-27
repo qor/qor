eec8b45009926cc9dd01e715d01973289a51f5eb
Jinzhu <wosmvp@gmail.com>
2015-01-27 15:25:30 +0800

    Fix some publish logic

--- publish/resolver.go
+++ publish/resolver.go
@@ -31,29 +31,29 @@ func IncludeValue(value string, values []string) bool {
 
 func (resolver *Resolver) AddDependency(dependency *Dependency) {
 	name := dependency.Type.String()
-	var primaryKeys []string
+	var newPrimaryKeys []string
 
 	if dep, ok := resolver.Dependencies[name]; ok {
 		for _, primaryKey := range dependency.PrimaryKeys {
 			if !IncludeValue(primaryKey, dep.PrimaryKeys) {
-				primaryKeys = append(primaryKeys, primaryKey)
+				newPrimaryKeys = append(newPrimaryKeys, primaryKey)
 				dep.PrimaryKeys = append(dep.PrimaryKeys, primaryKey)
 			}
 		}
 	} else {
 		resolver.Dependencies[name] = dependency
-		primaryKeys = dependency.PrimaryKeys
+		newPrimaryKeys = dependency.PrimaryKeys
 	}
 
-	if len(primaryKeys) > 0 {
-		resolver.GetDependencies(dependency, primaryKeys)
+	if len(newPrimaryKeys) > 0 {
+		resolver.GetDependencies(dependency, newPrimaryKeys)
 	}
 }
 
 func (resolver *Resolver) GetDependencies(dependency *Dependency, primaryKeys []string) {
 	// new slice
 	value := reflect.New(dependency.Type)
-	fromScope := resolver.DB.DB.NewScope(value.Addr().Interface())
+	fromScope := resolver.DB.DB.NewScope(value.Interface())
 	for _, field := range fromScope.Fields() {
 		if relationship := field.Relationship; relationship != nil {
 			toType := field.Field.Type()
@@ -63,7 +63,7 @@ func (resolver *Resolver) GetDependencies(dependency *Dependency, primaryKeys []
 			var err error
 
 			if relationship.Kind == "belongs_to" || relationship.Kind == "has_many" {
-				sql := fmt.Sprintf("%v IN (?) and publish_status = ?", relationship.ForeignKey)
+				sql := fmt.Sprintf("%v IN (?) and publish_status = ?", gorm.ToSnake(relationship.ForeignKey))
 				rows, err = resolver.DB.DB.Select(toScope.PrimaryKey()).Where(sql, primaryKeys, DIRTY).Rows()
 			} else if relationship.Kind == "has_one" {
 				fromTable := fromScope.TableName()
@@ -112,32 +112,33 @@ func (resolver *Resolver) Publish() {
 
 	for _, dependency := range resolver.Dependencies {
 		value := reflect.New(dependency.Type)
-		fromScope := resolver.DB.DraftMode().NewScope(value)
-		fromTable := fromScope.QuotedTableName()
-		fromPrimaryKey := fromScope.PrimaryKey()
-		toScope := resolver.DB.ProductionMode().NewScope(value)
-		toTable := toScope.QuotedTableName()
+		productionScope := resolver.DB.ProductionMode().NewScope(value.Interface())
+		productionTable := productionScope.TableName()
+		productionPrimaryKey := productionScope.PrimaryKey()
+		draftTable := DraftTableName(productionTable)
 
-		resolver.DB.ProductionMode().Delete(value.Interface(), dependency.PrimaryKeys)
+		resolver.DB.ProductionMode().Unscoped().Delete(value.Interface(), dependency.PrimaryKeys)
 
 		var columns []string
-		for _, field := range toScope.Fields() {
-			columns = append(columns, field.DBName)
+		for _, field := range productionScope.Fields() {
+			if field.IsNormal {
+				columns = append(columns, field.DBName)
+			}
 		}
 
 		var insertColumns []string
 		for _, column := range columns {
-			insertColumns = append(insertColumns, fmt.Sprintf("%v.%v", toTable, column))
+			insertColumns = append(insertColumns, fmt.Sprintf("%v.%v", productionTable, column))
 		}
 
 		var selectColumns []string
 		for _, column := range columns {
-			selectColumns = append(selectColumns, fmt.Sprintf("%v.%v", fromTable, column))
+			selectColumns = append(selectColumns, fmt.Sprintf("%v.%v", draftTable, column))
 		}
 
 		sql := fmt.Sprintf("INSERT INTO %v (%v) SELECT %v from %v where %v.%v in (?);",
-			toTable, strings.Join(insertColumns, " ,"), strings.Join(selectColumns, " ,"),
-			fromTable, fromTable, fromPrimaryKey)
+			productionTable, strings.Join(insertColumns, " ,"), strings.Join(selectColumns, " ,"),
+			draftTable, draftTable, productionPrimaryKey)
 
 		resolver.DB.DB.Exec(sql, dependency.PrimaryKeys)
 	}
