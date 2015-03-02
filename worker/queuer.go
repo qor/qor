d3188bb42368fa26df28c1ddc7d29bfbc2982a42
bom_d_van <bom.d.van@gmail.com>
2015-03-02 16:14:59 +0800

    worker: add Queuer comments

--- worker/queuer.go
+++ worker/queuer.go
@@ -1,8 +1,13 @@
 package worker
 
 type Queuer interface {
+	// Name returns the Queuer's identifier
 	Name() string
+	// Enqueue pushes a job to a queue, also a Queuer could set a id value (string)
+	// in QorJob's QueueJobId if needed
 	Enqueue(job *QorJob) (err error)
+	// Purge removes a job from a queue
 	Purge(job *QorJob) (err error)
+	// Dequeue blocks the process until a job id (and error if any) is returned
 	Dequeue() (jobId uint64, err error)
 }
