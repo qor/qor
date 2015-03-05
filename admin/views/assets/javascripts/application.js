43b20097dee48ff0b64e20ede608a3ea9059e880
Jinzhu <wosmvp@gmail.com>
2015-03-05 12:04:40 +0800

    Cleanup javascript from views

---
+++ admin/views/assets/javascripts/application.js
@@ -0,0 +1,39 @@
+$(function() {
+  $('.redactor-editor').redactor();
+
+
+  // crop images
+  var $image = $(".image-cropper");
+
+  $image.cropper({
+    done: function(data) {
+      console.log(data)
+    },
+    multiple: true,
+    zoomable: false,
+  });
+
+  if (window.URL) {
+    var $inputImage = $("input.image-cropper-upload"),
+      blobURL;
+
+    $inputImage.change(function () {
+      var files = this.files,
+      file;
+
+      if (files && files.length) {
+        file = files[0];
+
+        if (/^image\/\w+$/.test(file.type)) {
+          if (blobURL) {
+            URL.revokeObjectURL(blobURL); // Revoke the old one
+          }
+
+          blobURL = URL.createObjectURL(file);
+          $image.cropper("reset", true).cropper("replace", blobURL);
+          $inputImage.val("");
+        }
+      }
+    });
+  }
+});
