cb121f0cf5255192febf2fe78f4da134f926102c
Lancee Yggdrasil <ly.lancee@gmail.com>
2015-03-16 11:47:11 +0800

    add redactor clipper

--- admin/views/assets/javascripts/application.js
+++ admin/views/assets/javascripts/application.js
@@ -2,10 +2,27 @@ $(function() {
   'use strict';
 
   var $editors = $('.redactor-editor');
-  $editors.each(function() {
-    $(this).redactor({
-      imageUpload: $(this).data("upload-url"),
-      fileUpload: $(this).data("upload-url")
+  $editors.each(function(i, me) {
+    var cropURL = $(this).data('crop-url'), 
+        uploadURL = $(this).data("upload-url");
+
+    $(me).redactor({
+      plugins: ['clipper'],
+      imageUpload: uploadURL,
+      fileUpload: uploadURL,
+      initCallback: function() {
+        $(me).after('<div id="crop-data-wrapper"></div>');
+      },
+      imageUploadCallback: function($image) {
+        var src = $image[0].src;
+        var $cropDataHolder = $('<textarea id="redactor-crop-data'+ src +'" style="display:none">');
+
+        $('#crop-data-wrapper').append($cropDataHolder);
+
+      },
+      modalOpenedCallback: function(name) {
+
+      }
     });
   });
 
