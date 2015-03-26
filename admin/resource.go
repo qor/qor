3d1b5935f73b0fba493a5114b7152168e5ea667e
Jinzhu <wosmvp@gmail.com>
2015-03-26 11:12:50 +0800

    Extract default Saver,Deleter,Finder

--- admin/resource.go
+++ admin/resource.go
@@ -51,45 +51,57 @@ func (res *Resource) Decode(contextor qor.Contextor, value interface{}) (errs []
 	return resource.Decode(contextor, value, res)
 }
 
+func (res *Resource) DefaultFinder(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
+	var primaryKey string
+	if metaValues == nil {
+		primaryKey = context.ResourceID
+	} else if id := metaValues.Get(res.PrimaryFieldName()); id != nil {
+		primaryKey = utils.ToString(id.Value)
+	}
+
+	if primaryKey != "" {
+		if metaValues != nil {
+			if destroy := metaValues.Get("_destroy"); destroy != nil {
+				if fmt.Sprintf("%v", destroy.Value) != "0" {
+					context.GetDB().Delete(result, primaryKey)
+					return resource.ErrProcessorSkipLeft
+				}
+			}
+		}
+		return context.GetDB().First(result, primaryKey).Error
+	}
+	return nil
+}
+
 func (res *Resource) CallFinder(result interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
 	if res.Finder != nil {
 		return res.Finder(result, metaValues, context)
 	} else {
-		var primaryKey string
-		if metaValues == nil {
-			primaryKey = context.ResourceID
-		} else if id := metaValues.Get(res.PrimaryFieldName()); id != nil {
-			primaryKey = utils.ToString(id.Value)
-		}
-
-		if primaryKey != "" {
-			if metaValues != nil {
-				if destroy := metaValues.Get("_destroy"); destroy != nil {
-					if fmt.Sprintf("%v", destroy.Value) != "0" {
-						context.GetDB().Delete(result, primaryKey)
-						return resource.ErrProcessorSkipLeft
-					}
-				}
-			}
-			return context.GetDB().First(result, primaryKey).Error
-		}
-		return nil
+		return res.DefaultFinder(result, metaValues, context)
 	}
+}
+
+func (res *Resource) DefaultSearcher(result interface{}, context *qor.Context) error {
+	return context.GetDB().Set("gorm:order_by_primary_key", "DESC").Find(result).Error
 }
 
 func (res *Resource) CallSearcher(result interface{}, context *qor.Context) error {
 	if res.Searcher != nil {
 		return res.Searcher(result, context)
 	} else {
-		return context.GetDB().Set("gorm:order_by_primary_key", "DESC").Find(result).Error
+		return res.DefaultSearcher(result, context)
 	}
+}
+
+func (res *Resource) DefaultSaver(result interface{}, context *qor.Context) error {
+	return context.GetDB().Save(result).Error
 }
 
 func (res *Resource) CallSaver(result interface{}, context *qor.Context) error {
 	if res.Saver != nil {
 		return res.Saver(result, context)
 	} else {
-		return context.GetDB().Save(result).Error
+		return res.DefaultSaver(result, context)
 	}
 }
 
