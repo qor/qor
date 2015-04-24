2f28b0e452be72c922c708dbb91ce613393aad3f
Fengyuan Chen <cfy.net@gmail.com>
2015-04-24 11:12:37 +0800

    Add label filter

--- admin/views/assets/javascripts/qor.js
+++ admin/views/assets/javascripts/qor.js
@@ -1292,6 +1292,166 @@
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as anonymous module.
+    define('qor-filter', ['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node / CommonJS
+    factory(require('jquery'));
+  } else {
+    // Browser globals.
+    factory(jQuery);
+  }
+})(function ($) {
+
+  'use strict';
+
+  var location = window.location,
+
+      QorFilter = function (element, options) {
+        this.$element = $(element);
+        this.options = $.extend(true, {}, QorFilter.DEFAULTS, options);
+        this.init();
+      };
+
+  function encodeSearch(data, removed) {
+    var search = location.search,
+        params = [],
+        source;
+
+    if ($.isPlainObject(data)) {
+      source = decodeSearch(search);
+
+      $.each(data, function (i, n) {
+        if (removed) {
+          if (source.hasOwnProperty(i)) {
+            delete source[i];
+          }
+        } else {
+          source[i] = n;
+        }
+      });
+
+      $.each(source, function (i, n) {
+        params.push(typeof n === 'undefined' ? i : [i, n].join('='));
+      });
+
+      search = '?' + params.join('&');
+    }
+
+    return search;
+  }
+
+  function decodeSearch(search) {
+    var data = {},
+        params = [];
+
+    if (search && search.indexOf('?') > -1) {
+      search = search.split('?')[1];
+
+      if (search && search.indexOf('#') > -1) {
+        search = search.split('#')[0];
+      }
+
+      if (search) {
+        params = search.split('&');
+      }
+
+      $.each(params, function (i, param) {
+        param = param.split('=');
+        data[param[0]] = param[1];
+      });
+    }
+
+    return data;
+  }
+
+  QorFilter.prototype = {
+    constructor: QorFilter,
+
+    init: function () {
+      var $this = this.$element,
+          options = this.options,
+          $toggle = $this.find(options.toggle);
+
+      if (!$toggle.length) {
+        return;
+      }
+
+      this.$toggle = $toggle;
+      this.parse();
+      this.bind();
+    },
+
+    bind: function () {
+      this.$element.on('click', this.options.toggle, $.proxy(this.toggle, this));
+    },
+
+    parse: function () {
+      var options = this.options,
+          data = decodeSearch(location.search);
+
+      this.$toggle.each(function () {
+        var $this = $(this),
+            params = decodeSearch($this.attr('href'));
+
+        $.each(data, function (name) {
+          var matched = false;
+
+          $.each(params, function (i) {
+            if (i === name) {
+              matched = true;
+              return false;
+            }
+          });
+
+          $this.toggleClass(options.activeClass, matched);
+
+          if (matched) {
+            return false;
+          }
+        });
+      });
+    },
+
+    toggle: function (e) {
+      var $target = $(e.target),
+          data = decodeSearch(e.target.href),
+          search;
+
+      e.preventDefault();
+
+      if ($target.hasClass(this.options.activeClass)) {
+        search = encodeSearch(data, true);
+      } else {
+        search = encodeSearch(data);
+      }
+
+      location.search = search;
+    }
+  };
+
+  QorFilter.DEFAULTS = {
+    toggle: '',
+    activeClass: 'active'
+  };
+
+  $(function () {
+    $('.qor-label-group').each(function () {
+      var $this = $(this);
+
+      if (!$this.data('qor.filter')) {
+        $this.data('qor.filter', new QorFilter(this, {
+          toggle: '.label',
+          activeClass: 'label-primary'
+        }));
+      }
+    });
+  });
+
+});
+
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as anonymous module.
     define('qor-redactor', ['jquery'], factory);
   } else if (typeof exports === 'object') {
     // Node / CommonJS
