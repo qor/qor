06f43a1fae9f592eb0d49daf870f7cb714f9587f
Fengyuan Chen <cfy.net@gmail.com>
2015-04-28 14:55:37 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,41 @@
 
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')(),
+    path = function (dir) {
+      return ('admin/views/assets/' + (dir || ''));
+    },
     scripts = {
-      src: ['admin/views/assets/javascripts/components/*.js'],
-      dest: 'admin/views/assets/javascripts',
-      lib: 'admin/views/assets/javascripts/lib',
-      main: 'admin/views/assets/javascripts/main.js'
+      src: [
+        path('javascripts/app/components/*.js'),
+        path('javascripts/app/*.js')
+      ],
+      dest: path('javascripts'),
+      all: path('javascripts/app/**/*.js'),
+      lib: path('javascripts/lib')
     },
     styles = {
-      src: ['admin/views/assets/stylesheets/scss/**/*.scss'],
-      dest: 'admin/views/assets/stylesheets',
-      lib: 'admin/views/assets/stylesheets/lib',
-      main: 'admin/views/assets/stylesheets/scss/main.scss'
+      src: path('stylesheets/scss/*.scss'),
+      dest: path('stylesheets'),
+      css: path('stylesheets/*.css'),
+      all: path('stylesheets/scss/**/*.scss'),
+      lib: path('stylesheets/lib'),
+      fonts: path('stylesheets/fonts')
     };
 
 gulp.task('jshint', function () {
   return gulp.src(scripts.src)
-  .pipe(plugins.jshint('admin/views/assets/javascripts/.jshintrc'))
+  .pipe(plugins.jshint(path('javascripts/app/.jshintrc')))
   .pipe(plugins.jshint.reporter('default'));
 });
 
 gulp.task('jscs', function () {
   return gulp.src(scripts.src)
-  .pipe(plugins.jscs('admin/views/assets/javascripts/.jscsrc'));
+  .pipe(plugins.jscs(path('javascripts/app/.jscsrc')));
 });
 
 gulp.task('js', ['jshint', 'jscs'], function () {
   return gulp.src(scripts.src)
-  .pipe(plugins.concat('main.js'))
+  .pipe(plugins.concat('app.js'))
   .pipe(plugins.uglify())
   .pipe(gulp.dest(scripts.dest));
 });
@@ -36,68 +44,75 @@ gulp.task('js', ['jshint', 'jscs'], function () {
 gulp.task('concat', function () {
   return gulp.src(scripts.src)
   .pipe(plugins.sourcemaps.init())
-  .pipe(plugins.concat('main.js'))
+  .pipe(plugins.concat('app.js'))
   .pipe(plugins.sourcemaps.write('./'))
-  .pipe(gulp.dest(scripts.dest))
+  .pipe(gulp.dest(scripts.dest));
 });
 
 gulp.task('jslib', function () {
   return gulp.src([
-    'bower_components/jquery/dist/jquery.js',
     'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/bootstrap/dist/js/bootstrap.js',
     'bower_components/bootstrap/dist/js/bootstrap.min.js',
-    'bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+    'bower_components/redactor/redactor.min.js',
+    'bower_components/cropper/dist/cropper.min.js',
     'bower_components/chosen/chosen.jquery.min.js'
   ])
-  .pipe(gulp.dest(scripts.lib))
+  .pipe(gulp.dest(scripts.lib));
 });
 
-gulp.task('css', function () {
-  return gulp.src(styles.main)
+gulp.task('sass', function () {
+  return gulp.src(styles.src)
+  .pipe(plugins.sourcemaps.init())
   .pipe(plugins.sass())
-  .pipe(plugins.csslint())
+  .pipe(plugins.sourcemaps.write('./'))
+  .pipe(gulp.dest(styles.dest));
+});
+
+gulp.task('csslint', ['sass'], function () {
+  return gulp.src(styles.css)
+  .pipe(plugins.csslint(path('stylesheets/scss/.csslintrc')))
+  .pipe(plugins.csslint.reporter());
+});
+
+gulp.task('css', ['csslint'], function () {
+  return gulp.src(styles.css)
   .pipe(plugins.autoprefixer())
+  .pipe(plugins.csscomb())
   .pipe(plugins.minifyCss())
   .pipe(gulp.dest(styles.dest));
 });
 
-gulp.task('sass', function () {
-  return gulp.src(styles.main)
-  .pipe(plugins.sass())
-  .pipe(gulp.dest(styles.dest))
-});
-
 gulp.task('fonts', function () {
   return gulp.src([
-    'bower_components/bootstrap/fonts/*',
+    'bower_components/bootstrap/fonts/*'
   ])
-  .pipe(gulp.dest('admin/views/assets/fonts'))
+  .pipe(gulp.dest(styles.fonts));
 });
 
-gulp.task('csslib', ['fonts'], function () {
+gulp.task('redactor', function () {
+  return gulp.src('bower_components/redactor/redactor.css')
+  .pipe(plugins.rename('redactor.min.css'))
+  .pipe(plugins.minifyCss())
+  .pipe(gulp.dest('bower_components/redactor'));
+});
+
+gulp.task('csslib', ['fonts', 'redactor'], function () {
   return gulp.src([
-    'bower_components/bootstrap/dist/css/bootstrap.css',
-    'bower_components/bootstrap/dist/css/bootstrap.css.map',
     'bower_components/bootstrap/dist/css/bootstrap.min.css',
-    'bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css',
+    'bower_components/redactor/redactor.min.css',
+    'bower_components/cropper/dist/cropper.min.css',
     'bower_components/chosen/chosen-sprite.png',
     'bower_components/chosen/chosen-sprite@2x.png',
     'bower_components/chosen/chosen.min.css'
   ])
-  .pipe(gulp.dest(styles.lib))
-});
-
-gulp.task('redactor', function () {
-  return gulp.src('admin/views/assets/stylesheets/lib/redactor.css')
-  .pipe(plugins.rename('redactor.min.css'))
-  .pipe(plugins.minifyCss())
-  .pipe(gulp.dest('admin/views/assets/stylesheets/lib'));
+  .pipe(gulp.dest(styles.lib));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(scripts.src, ['concat']);
-  gulp.watch(styles.src, ['sass']);
+  gulp.watch(scripts.all, ['concat']);
+  gulp.watch(styles.all, ['sass']);
 });
+
+gulp.task('release', ['js', 'css']);
 
 gulp.task('default', ['watch']);
