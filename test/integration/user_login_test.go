8b0028041e3ae0bb2ac2a936144ab070df3c761e
Raven <enn.raven@gmail.com>
2015-05-06 17:17:53 +0800

    UpdateUser and DeleteUser test.

--- test/integration/user_login_test.go
+++ test/integration/user_login_test.go
@@ -31,3 +31,44 @@ func TestCreateUser(t *testing.T) {
 		t.Error("user not created")
 	}
 }
+
+func TestUpdateUser(t *testing.T) {
+	defer StopDriverOnPanic()
+
+	user := &User{Name: "old name"}
+	DB.Create(&user)
+	newUserName := "new name"
+
+	Expect(page.Navigate(fmt.Sprintf("%v/user", baseUrl))).To(Succeed())
+
+	editLinkSelector := fmt.Sprintf("a[href='/admin/user/%v'].md-edit", user.ID)
+	Expect(page.Find(editLinkSelector).Click()).To(Succeed())
+
+	page.Find("#QorResourceName").Fill(newUserName)
+	page.FindByButton("Save").Click()
+
+	DB.First(&user, user.ID)
+
+	if user.Name != newUserName {
+		t.Error("user not updated")
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	defer StopDriverOnPanic()
+
+	user := &User{Name: "old name"}
+	DB.Create(&user)
+
+	Expect(page.Navigate(fmt.Sprintf("%v/user", baseUrl))).To(Succeed())
+
+	deleteLinkSelector := fmt.Sprintf("form[action='/admin/user/%v'] button.md-delete", user.ID)
+	Expect(page.Find(deleteLinkSelector).Click()).To(Succeed())
+	Expect(page.ConfirmPopup()).To(Succeed())
+
+	err := DB.First(&user, user.ID).Error
+
+	if err == nil {
+		t.Error("user not deleted")
+	}
+}
