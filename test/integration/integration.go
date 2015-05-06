03cf5c6c9c93cf36923a34000039cb2728d792e9
Raven <enn.raven@gmail.com>
2015-05-06 11:01:05 +0800

    Change integration test db driver to mysql.

--- test/integration/integration.go
+++ test/integration/integration.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/qor/qor"
 	"github.com/qor/qor/admin"
 )
@@ -21,9 +21,21 @@ type Product struct {
 	Description string
 }
 
+var DB gorm.DB
+
 func Start(port int) {
-	DB, _ := gorm.Open("sqlite3", "demo.db")
-	DB.AutoMigrate(&User{}, &Product{})
+	var err error
+	// CREATE USER 'qor'@'localhost' IDENTIFIED BY 'qor';
+	// CREATE DATABASE qor_integration_test;
+	// GRANT ALL PRIVILEGES ON qor_integration_test.* TO 'qor'@'localhost';
+	dbuser, dbpwd, dbname := "qor", "qor", "qor_integration_test"
+
+	DB, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", dbuser, dbpwd, dbname))
+	if err != nil {
+		panic(err)
+	}
+
+	setupDb()
 
 	Admin := admin.New(&qor.Config{DB: &DB})
 	Admin.AddResource(&User{}, &admin.Config{Menu: []string{"User Management"}})
@@ -32,4 +44,25 @@ func Start(port int) {
 	mux := http.NewServeMux()
 	Admin.MountTo("/admin", mux)
 	http.ListenAndServe(fmt.Sprintf(":%v", port), mux)
+}
+
+func getTables() []interface{} {
+	return []interface{}{
+		&User{},
+		&Product{},
+	}
+}
+
+func setupDb() {
+	tables := getTables()
+
+	for _, table := range tables {
+		if err := DB.DropTableIfExists(table).Error; err != nil {
+			panic(err)
+		}
+
+		if err := DB.AutoMigrate(table).Error; err != nil {
+			panic(err)
+		}
+	}
 }
