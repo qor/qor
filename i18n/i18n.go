788e9cdd5a5afa8fceba0a20316236eb42eba3b5
Jinzhu <wosmvp@gmail.com>
2015-05-22 15:04:15 +0800

    Add Get Available/Editable locales funcmap

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -2,13 +2,18 @@ package i18n
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"path"
 	"strings"
 
+	"github.com/qor/qor"
 	"github.com/qor/qor/admin"
+	"github.com/qor/qor/utils"
 	"github.com/theplant/cldr"
 )
+
+var Default = "en-US"
 
 type I18n struct {
 	Backends     []Backend
@@ -76,6 +81,48 @@ func (i18n *I18n) T(locale, key string, args ...interface{}) string {
 	return key
 }
 
+func getLocaleFromContext(context *qor.Context) string {
+	if locale := utils.GetLocale(context); locale != "" {
+		return locale
+	}
+
+	return Default
+}
+
+type AvailableLocalesInterface interface {
+	AvailableLocales() []string
+}
+
+type ViewableLocalesInterface interface {
+	ViewableLocales() []string
+}
+
+type EditableLocalesInterface interface {
+	EditableLocales() []string
+}
+
+func GetAvailableLocales(req *http.Request, currentUser qor.CurrentUser) []string {
+	if user, ok := currentUser.(ViewableLocalesInterface); ok {
+		return user.ViewableLocales()
+	}
+
+	if user, ok := currentUser.(AvailableLocalesInterface); ok {
+		return user.AvailableLocales()
+	}
+	return []string{}
+}
+
+func GetEditableLocales(req *http.Request, currentUser qor.CurrentUser) []string {
+	if user, ok := currentUser.(EditableLocalesInterface); ok {
+		return user.EditableLocales()
+	}
+
+	if user, ok := currentUser.(AvailableLocalesInterface); ok {
+		return user.AvailableLocales()
+	}
+	return []string{}
+}
+
 func (i18n *I18n) InjectQorAdmin(res *admin.Resource) {
 	res.Config.Theme = "i18n"
 	res.GetAdmin().I18n = i18n
@@ -83,6 +130,22 @@ func (i18n *I18n) InjectQorAdmin(res *admin.Resource) {
 	controller := I18nController{i18n}
 	router := res.GetAdmin().GetRouter()
 	router.Get(fmt.Sprintf("^/%v", res.ToParam()), controller.Index)
+
+	res.GetAdmin().RegisterFuncMap("i18n_primary_locale", func(context admin.Context) string {
+		return getLocaleFromContext(context.Context)
+	})
+
+	res.GetAdmin().RegisterFuncMap("i18n_edting_locale", func(context admin.Context) string {
+		return getLocaleFromContext(context.Context)
+	})
+
+	res.GetAdmin().RegisterFuncMap("i18n_viewable_locales", func(context admin.Context) []string {
+		return GetAvailableLocales(context.Request, context.CurrentUser)
+	})
+
+	res.GetAdmin().RegisterFuncMap("i18n_editable_locales", func(context admin.Context) []string {
+		return GetEditableLocales(context.Request, context.CurrentUser)
+	})
 
 	for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
 		admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/qor/i18n/views"))
---
+++ i18n/views/assets/javascripts/translation.js
@@ -0,0 +1,1 @@
+// TODO just works ™
