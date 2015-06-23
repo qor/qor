14ba8f4a608a2a0fb1ddf75c24ed77b866c62f07
Jinzhu <wosmvp@gmail.com>
2015-06-23 16:33:27 +0800

    Auto create join table data

--- publish/many_2_many.go
+++ publish/many_2_many.go
@@ -13,3 +13,23 @@ func (handler PublishJoinTableHandler) Table(db *gorm.DB) string {
 		return handler.TableName
 	}
 }
+
+func (handler PublishJoinTableHandler) Add(h gorm.JoinTableHandlerInterface, db *gorm.DB, source1 interface{}, source2 interface{}) error {
+	// production mode
+	if draftMode, ok := db.Get("publish:draft_mode"); !ok || !draftMode.(bool) {
+		if err := handler.JoinTableHandler.Add(h, db.Set("publish:draft_mode", false), source1, source2); err != nil {
+			return err
+		}
+	}
+	return handler.JoinTableHandler.Add(h, db, source1, source2)
+}
+
+func (handler PublishJoinTableHandler) Delete(h gorm.JoinTableHandlerInterface, db *gorm.DB, sources ...interface{}) error {
+	// production mode
+	if draftMode, ok := db.Get("publish:draft_mode"); !ok || !draftMode.(bool) {
+		if err := handler.JoinTableHandler.Delete(h, db.Set("publish:draft_mode", false), sources...); err != nil {
+			return err
+		}
+	}
+	return handler.JoinTableHandler.Delete(h, db, sources...)
+}
