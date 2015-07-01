3fa123e1115331b559da29e52718940548078be5
Florian von Bock <f@vonbock.info>
2015-07-01 12:10:22 +0900

    clean up after move

--- example/tutorial/bookstore/01/handlers.go
+++ example/tutorial/bookstore/01/handlers.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+// books
 func listBooksHandler(ctx *gin.Context) {
 	var books []*Book
 
@@ -45,4 +48,35 @@ func viewBookHandler(ctx *gin.Context) {
 			"book": book,
 		},
 	)
+}
+
+// simple login - username only/no passwords yet
+// TODO: switch this to gin too?
+func loginHandler(writer http.ResponseWriter, request *http.Request) {
+	var user User
+
+	if request.Method == "POST" {
+		request.ParseForm()
+		if !db.First(&user, "name = ?", request.Form.Get("username")).RecordNotFound() {
+			cookie := http.Cookie{Name: "userid", Value: fmt.Sprintf("%v", user.ID), Expires: time.Now().AddDate(1, 0, 0)}
+			http.SetCookie(writer, &cookie)
+			writer.Write([]byte("<html><body>logged in as `" + user.Name + "`, go to <a href='/admin'>admin</a></body></html>"))
+		} else {
+			http.Redirect(writer, request, "/login?failed_to_login", 301)
+		}
+	} else if userid, err := request.Cookie("userid"); err == nil {
+		if !db.First(&user, "id = ?", userid.Value).RecordNotFound() {
+			writer.Write([]byte("<html><body>already logged as `" + user.Name + "`, go <a href='/admin'>admin</a></body></html>"))
+		} else {
+			http.Redirect(writer, request, "/logout", http.StatusSeeOther)
+		}
+	} else {
+		writer.Write([]byte(`<html><form action="/login" method="POST"><input name="username" value="" placeholder="username"><input type=submit value="Login"></form></html>`))
+	}
+}
+
+func logoutHandler(writer http.ResponseWriter, request *http.Request) {
+	cookie := http.Cookie{Name: "userid", MaxAge: -1}
+	http.SetCookie(writer, &cookie)
+	http.Redirect(writer, request, "/login?logged_out", http.StatusSeeOther)
 }
--- example/tutorial/bookstore/01/main.go
+++ example/tutorial/bookstore/01/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/qor/qor"
@@ -41,6 +40,7 @@ func main() {
 		},
 	)
 
+	// alternate price display
 	book.Meta(&admin.Meta{
 		Name: "DisplayPrice",
 		Valuer: func(value interface{}, context *qor.Context) interface{} {
@@ -52,22 +52,10 @@ func main() {
 		},
 	})
 
-	// what fields should be displayed in the books list on admin
-	book.IndexAttrs("Title", "Authors", "ReleaseDate", "DisplayPrice")
-
-	// defines the edit field for authors of the book
+	// defines the display field for authors in the product list
 	book.Meta(&admin.Meta{
-		Name:  "Authors",
+		Name:  "AuthorNames",
 		Label: "Authors",
-		Type:  "select_many",
-		// Collection: func(resource interface{}, context *qor.Context) (results [][]string) {
-		// 	if authors := []Author{}; !context.GetDB().Find(&authors).RecordNotFound() {
-		// 		for _, author := range authors {
-		// 			results = append(results, []string{fmt.Sprintf("%v", author.ID), author.Name})
-		// 		}
-		// 	}
-		// 	return
-		// },
 		Valuer: func(value interface{}, context *qor.Context) interface{} {
 			if value == nil {
 				return value
@@ -88,6 +76,10 @@ func main() {
 			return authors
 		},
 	})
+
+	// what fields should be displayed in the books list on admin
+	book.IndexAttrs("Title", "AuthorNames", "ReleaseDate", "DisplayPrice")
+	// what fields should be editable in the book esit interface
 	book.EditAttrs("Title", "Authors", "Synopsis", "ReleaseDate", "Price", "CoverImage")
 
 	mux := http.NewServeMux()
@@ -97,11 +89,11 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
-	// Chapter 3: serve static files
+	// serve static files
 	router.StaticFS("/system/", http.Dir("public/system"))
 	router.StaticFS("/assets/", http.Dir("public/assets"))
 
-	// all books - listing
+	// books - listing
 	router.GET("/books", listBooksHandler)
 	// single book - product page
 	router.GET("/books/:id", viewBookHandler)
@@ -110,35 +102,9 @@ func main() {
 
 	// handle login and logout of users
 	Admin.SetAuth(&Auth{})
+	mux.HandleFunc("/login", loginHandler)
+	mux.HandleFunc("/logout", logoutHandler)
 
-	mux.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
-		var user User
-
-		if request.Method == "POST" {
-			request.ParseForm()
-			if !db.First(&user, "name = ?", request.Form.Get("username")).RecordNotFound() {
-				cookie := http.Cookie{Name: "userid", Value: fmt.Sprintf("%v", user.ID), Expires: time.Now().AddDate(1, 0, 0)}
-				http.SetCookie(writer, &cookie)
-				writer.Write([]byte("<html><body>logged in as `" + user.Name + "`, go to <a href='/admin'>admin</a></body></html>"))
-			} else {
-				http.Redirect(writer, request, "/login?failed_to_login", 301)
-			}
-		} else if userid, err := request.Cookie("userid"); err == nil {
-			if !db.First(&user, "id = ?", userid.Value).RecordNotFound() {
-				writer.Write([]byte("<html><body>already logged as `" + user.Name + "`, go <a href='/admin'>admin</a></body></html>"))
-			} else {
-				http.Redirect(writer, request, "/logout", http.StatusSeeOther)
-			}
-		} else {
-			writer.Write([]byte(`<html><form action="/login" method="POST"><input name="username" value="" placeholder="username"><input type=submit value="Login"></form></html>`))
-		}
-	})
-
-	mux.HandleFunc("/logout", func(writer http.ResponseWriter, request *http.Request) {
-		cookie := http.Cookie{Name: "userid", MaxAge: -1}
-		http.SetCookie(writer, &cookie)
-		http.Redirect(writer, request, "/login?logged_out", http.StatusSeeOther)
-	})
-
+	// start the server
 	http.ListenAndServe(":9000", mux)
 }
