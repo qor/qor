8016ca505f0e4a494b9fe952b1a094ea48d8a16d
Jinzhu <wosmvp@gmail.com>
2015-07-03 16:50:44 +0800

    Move qor.ExitWithMsg to utils

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"runtime"
+	"runtime/debug"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -138,4 +141,28 @@ func ParseTagOption(str string) map[string]string {
 		}
 	}
 	return setting
+}
+
+func filenameWithLineNum() string {
+	var total = 10
+	var results []string
+	for i := 2; i < 15; i++ {
+		if _, file, line, ok := runtime.Caller(i); ok {
+			total--
+			results = append(results[:0],
+				append(
+					[]string{fmt.Sprintf("%v:%v", strings.TrimPrefix(file, os.Getenv("GOPATH")+"src/"), line)},
+					results[0:len(results)]...)...)
+
+			if total == 0 {
+				return strings.Join(results, "\n")
+			}
+		}
+	}
+	return ""
+}
+
+func ExitWithMsg(str string, value ...interface{}) {
+	fmt.Printf("\n"+filenameWithLineNum()+"\n"+str+"\n", value...)
+	debug.PrintStack()
 }
