50d406c835c4d8dbaba4e6f17a2b69ae65856a9c
Florian von Bock <f@vonbock.info>
2015-07-08 11:57:10 +0900

    fix table creation for translations on mysql when using a database with DEFAULT CHARACTER SET utf8mb4 on mysql the table creation will fail when not limited in size for key,locale: the compound primary key would exceed the size limit: Error 1071: Specified key was too long; max key length is 767 bytes ideally we'd want to only limit the length to which the index uses the fields, but 190/20 should be sufficient for keys and locales.

--- i18n/backends/database/database.go
+++ i18n/backends/database/database.go
@@ -5,9 +5,16 @@ import (
 	"github.com/qor/qor/i18n"
 )
 
+/* when using a database with DEFAULT CHARACTER SET utf8mb4 on mysql the table
+creation will fail when not limited in size for key,locale: the compound primary
+key would exceed the size limit:
+Error 1071: Specified key was too long; max key length is 767 bytes
+ideally we'd want to only limit the length to which the index uses the fields,
+but 190/20 should be sufficient for keys and locales.
+*/
 type Translation struct {
-	Key    string `gorm:"primary_key"`
-	Locale string `gorm:"primary_key"`
+	Key    string `gorm:"primary_key" sql:"size:190"`
+	Locale string `gorm:"primary_key" sql:"size:20"`
 	Value  string `sql:"size:4294967295"`
 }
 
