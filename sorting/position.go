07b0141d771fb4362a08172e49bbca83146b3108
Jinzhu <wosmvp@gmail.com>
2015-07-16 15:14:24 +0800

    Add dummy PositionMoveUp, PositionMoveDown, PositionMoveTo method

--- sorting/position.go
+++ sorting/position.go
@@ -5,10 +5,20 @@ type Position struct {
 }
 
 func (Position) PositionMoveUp(pos int) {
+	// update other's position: (current position + 1 .. current position + pos) - 1
+	// update self's position: current position + pos
 }
 
 func (Position) PositionMoveDown(pos int) {
+	// update other's position: (current position + 1 .. current position + pos) + 1
+	// update self's position: current position - pos
 }
 
 func (Position) PositionMoveTo(pos int) {
+	// if pos < current position
+	//   update other's position: (pos .. current position) + 1
+	//   update self's position: pos
+	// else if pos > current position
+	//   update other's position: (current position .. pos) - 1
+	//   update self's position: pos
 }
