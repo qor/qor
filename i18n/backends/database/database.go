b7d4eed0cec9982409ad5e4de7da46a36febbfc0
Florian von Bock <f@vonbock.info>
2015-07-16 12:49:46 +0900

    return error from db op

--- i18n/backends/database/database.go
+++ i18n/backends/database/database.go
@@ -45,7 +45,8 @@ func (backend *Backend) SaveTranslation(t *i18n.Translation) (err error) {
 		err = fmt.Errorf("Translation locale is too long: %v. Maximum locale length is 8: %s", len(t.Locale), t.Locale)
 		return
 	}
-	err = backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Assign(Translation{Value: t.Value}).FirstOrCreate(&Translation{})
+	backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Assign(Translation{Value: t.Value}).FirstOrCreate(&Translation{})
+	err = backend.DB.Error
 	return
 }
 
