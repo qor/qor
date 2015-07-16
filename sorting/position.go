83a953e658c13bf1a15e059acb8b9c1dcb09989a
Jinzhu <wosmvp@gmail.com>
2015-07-16 15:07:28 +0800

    Add sorting package

---
+++ sorting/position.go
@@ -0,0 +1,14 @@
+package sorting
+
+type Position struct {
+	Position int
+}
+
+func (Position) PositionMoveUp(pos int) {
+}
+
+func (Position) PositionMoveDown(pos int) {
+}
+
+func (Position) PositionMoveTo(pos int) {
+}
