0e5b63ab4c09f81bc819c8c4f57a7bdfe4287dc8
Jinzhu <wosmvp@gmail.com>
2015-07-16 15:30:46 +0800

    Update sorting package

--- sorting/position.go
+++ sorting/position.go
@@ -1,20 +1,33 @@
 package sorting
 
+type positionInterface interface {
+	GetPosition() int
+	SetPosition(int)
+}
+
 type Position struct {
 	Position int
 }
 
-func (Position) PositionMoveUp(pos int) {
+func (position Position) GetPosition() int {
+	return position.Position
+}
+
+func (position Position) SetPosition(pos int) {
+	position.Position = pos
+}
+
+func MoveUp(value positionInterface, pos int) {
 	// update other's position: (current position + 1 .. current position + pos) - 1
 	// update self's position: current position + pos
 }
 
-func (Position) PositionMoveDown(pos int) {
+func MoveDown(value positionInterface, pos int) {
 	// update other's position: (current position + 1 .. current position + pos) + 1
 	// update self's position: current position - pos
 }
 
-func (Position) PositionMoveTo(pos int) {
+func MoveTo(value positionInterface, pos int) {
 	// if pos < current position
 	//   update other's position: (pos .. current position) + 1
 	//   update self's position: pos
