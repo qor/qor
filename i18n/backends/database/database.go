65bcbe5a33593a3c6043ceb42a08c1d660b16012
Jinzhu <wosmvp@gmail.com>
2015-07-21 11:05:54 +0800

    Refactor i18n

--- i18n/backends/database/database.go
+++ i18n/backends/database/database.go
@@ -6,13 +6,15 @@ import (
 )
 
 type Translation struct {
-	Key    string `gorm:"primary_key"`
-	Locale string `gorm:"primary_key"`
+	Locale string `sql:"size:12;"`
+	Key    string `sql:"size:4294967295;"`
 	Value  string `sql:"size:4294967295"`
 }
 
 func New(db *gorm.DB) i18n.Backend {
 	db.AutoMigrate(&Translation{})
+	quotedKey := db.NewScope(&Translation{}).Quote("key") + "(190)"
+	db.Model(&Translation{}).AddUniqueIndex("idx_translations_key_with_locale", "locale", quotedKey)
 	return &Backend{DB: db}
 }
 
@@ -20,16 +22,17 @@ type Backend struct {
 	DB *gorm.DB
 }
 
-func (backend *Backend) LoadTranslations() []*i18n.Translation {
-	var translations []*i18n.Translation
+func (backend *Backend) LoadTranslations() (translations []*i18n.Translation) {
 	backend.DB.Find(&translations)
 	return translations
 }
 
-func (backend *Backend) SaveTranslation(t *i18n.Translation) {
-	backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Assign(Translation{Value: t.Value}).FirstOrCreate(&Translation{})
+func (backend *Backend) SaveTranslation(t *i18n.Translation) error {
+	return backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).
+		Assign(Translation{Value: t.Value}).
+		FirstOrCreate(&Translation{}).Error
 }
 
-func (backend *Backend) DeleteTranslation(t *i18n.Translation) {
-	backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Delete(&Translation{})
+func (backend *Backend) DeleteTranslation(t *i18n.Translation) error {
+	return backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Delete(&Translation{}).Error
 }
