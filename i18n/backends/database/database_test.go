1a47cdea5da541f4b89c984357a55dafd9db5385
Jinzhu <wosmvp@gmail.com>
2015-07-21 13:21:23 +0800

    Test long translation

--- i18n/backends/database/database_test.go
+++ i18n/backends/database/database_test.go
@@ -30,4 +30,18 @@ func TestTranslations(t *testing.T) {
 	if len(backend.LoadTranslations()) != 0 {
 		t.Errorf("should has none translation")
 	}
+
+	longText := "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+	backend.SaveTranslation(&i18n.Translation{Key: longText + "1", Value: longText, Locale: "zh-CN"})
+	backend.SaveTranslation(&i18n.Translation{Key: longText + "2", Value: longText, Locale: "zh-CN"})
+
+	if len(backend.LoadTranslations()) != 2 {
+		t.Errorf("should has two translations")
+	}
+
+	backend.DeleteTranslation(&i18n.Translation{Key: longText + "1", Value: longText, Locale: "zh-CN"})
+	if len(backend.LoadTranslations()) != 1 {
+		t.Errorf("should has one translation left")
+	}
 }
