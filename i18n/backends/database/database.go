664d6039b21e2db81172952a6a02121ab183a883
Jinzhu <wosmvp@gmail.com>
2015-07-21 10:13:20 +0800

    Add mutex lock for database operations

--- i18n/backends/database/database.go
+++ i18n/backends/database/database.go
@@ -1,23 +1,28 @@
 package database
 
 import (
+	"sync"
+
 	"github.com/jinzhu/gorm"
 	"github.com/qor/qor/i18n"
 )
 
 type Translation struct {
-	Key    string `gorm:"primary_key"`
-	Locale string `gorm:"primary_key"`
+	gorm.Model
+	Locale string `sql:"size:12;"`
+	Key    string `sql:"size:4294967295;"`
 	Value  string `sql:"size:4294967295"`
 }
 
 func New(db *gorm.DB) i18n.Backend {
 	db.AutoMigrate(&Translation{})
-	return &Backend{DB: db}
+	db.Model(&Translation{}).AddIndex("idx_translations_key_with_locale", "locale", "`key`(190)")
+	return &Backend{DB: db, mutex: &sync.Mutex{}}
 }
 
 type Backend struct {
-	DB *gorm.DB
+	DB    *gorm.DB
+	mutex *sync.Mutex
 }
 
 func (backend *Backend) LoadTranslations() []*i18n.Translation {
@@ -27,9 +32,13 @@ func (backend *Backend) LoadTranslations() []*i18n.Translation {
 }
 
 func (backend *Backend) SaveTranslation(t *i18n.Translation) {
+	backend.mutex.Lock()
 	backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Assign(Translation{Value: t.Value}).FirstOrCreate(&Translation{})
+	backend.mutex.Unlock()
 }
 
 func (backend *Backend) DeleteTranslation(t *i18n.Translation) {
+	backend.mutex.Lock()
 	backend.DB.Where(Translation{Key: t.Key, Locale: t.Locale}).Delete(&Translation{})
+	backend.mutex.Unlock()
 }
