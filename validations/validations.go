2417ee4237a68daa1336fa98e51ead34987d3402
Jinzhu <wosmvp@gmail.com>
2015-08-07 15:26:36 +0800

    Refactor validations

--- validations/validations.go
+++ validations/validations.go
@@ -6,24 +6,29 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-func AddError(db *gorm.DB, resource interface{}, column, err string) {
-	scope := db.NewScope(resource)
-	var errors = map[string]string{}
+var settingKey = "validations:errors"
 
-	if e, ok := db.Get("validations:errors"); ok {
-		errors = e.(map[string]string)
+func AddError(db *gorm.DB, resource interface{}, err string) {
+	scope := db.NewScope(resource)
+	var errors = map[string][]string{}
+
+	if e, ok := db.Get(settingKey); ok {
+		errors = e.(map[string][]string)
 	}
 
-	key := fmt.Sprintf("%v_%v_%v", scope.GetModelStruct().ModelType.Name(), scope.PrimaryKeyValue(), column)
-	errors[key] = err
+	key := fmt.Sprintf("%v::%v", scope.GetModelStruct().ModelType.Name(), scope.PrimaryKeyValue())
+	errors[key] = append(errors[key], err)
 
-	db.InstantSet("validations:errors", errors)
+	db.InstantSet(settingKey, errors)
 }
 
-func GetErrors(db *gorm.DB) map[string]string {
-	var errors = map[string]string{}
-	if e, ok := db.Get("validations:errors"); ok {
-		errors = e.(map[string]string)
+func AddErrorForColumn(db *gorm.DB, resource interface{}, column, err string) {
+}
+
+func GetErrors(db *gorm.DB) map[string][]string {
+	var errors = map[string][]string{}
+	if e, ok := db.Get(settingKey); ok {
+		errors = e.(map[string][]string)
 	}
 	return errors
 }
