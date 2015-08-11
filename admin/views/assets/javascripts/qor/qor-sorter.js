7fd929683c80350becf19cf12a49c60450870bcc
Fengyuan Chen <cfy.org@gmail.com>
2015-08-11 15:37:18 +0800

    Implement table columns sorting

---
+++ admin/views/assets/javascripts/qor/qor-sorter.js
@@ -0,0 +1,102 @@
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node / CommonJS
+    factory(require('jquery'));
+  } else {
+    // Browser globals.
+    factory(jQuery);
+  }
+})(function ($) {
+
+  'use strict';
+
+  var location = window.location;
+  var NAMESPACE = 'qor.sorter';
+  var EVENT_ENABLE = 'enable.' + NAMESPACE;
+  var EVENT_DISABLE = 'disable.' + NAMESPACE;
+  var EVENT_CLICK = 'click.' + NAMESPACE;
+
+  function QorSorter(element, options) {
+    this.$element = $(element);
+    this.options = $.extend({}, QorSorter.DEFAULTS, $.isPlainObject(options) && options);
+    this.init();
+  }
+
+  QorSorter.prototype = {
+    constructor: QorSorter,
+
+    init: function () {
+      this.bind();
+    },
+
+    bind: function () {
+      this.$element.on(EVENT_CLICK, '> thead > tr > th', $.proxy(this.sort, this));
+    },
+
+    unbind: function () {
+      this.$element.off(EVENT_CLICK, this.sort);
+    },
+
+    sort: function (e) {
+      var $target = $(e.currentTarget);
+      var search = location.search;
+      var param = 'order_by=' + $target.data('orderBy');
+
+      if (/order_by/.test(search)) {
+        search = search.replace(/order_by(=\w+)?/, function () {
+          return param;
+        });
+      } else {
+        search += search.indexOf('&') > -1 ? ('&' + param) : param;
+      }
+
+      location.search = search;
+    },
+
+    destroy: function () {
+      this.unbind();
+      this.$element.chosen('destroy');
+    },
+  };
+
+  QorSorter.DEFAULTS = {};
+
+  QorSorter.plugin = function (options) {
+    return this.each(function () {
+      var $this = $(this);
+      var data = $this.data(NAMESPACE);
+      var fn;
+
+      if (!data) {
+        if (/destroy/.test(options)) {
+          return;
+        }
+
+        $this.data(NAMESPACE, (data = new QorSorter(this, options)));
+      }
+
+      if (typeof options === 'string' && $.isFunction(fn = data[options])) {
+        fn.apply(data);
+      }
+    });
+  };
+
+  $(function () {
+    var selector = 'table';
+
+    $(document)
+      .on(EVENT_DISABLE, function (e) {
+        QorSorter.plugin.call($(selector, e.target), 'destroy');
+      })
+      .on(EVENT_ENABLE, function (e) {
+        QorSorter.plugin.call($(selector, e.target));
+      })
+      .triggerHandler(EVENT_ENABLE);
+  });
+
+  return QorSorter;
+
+});
