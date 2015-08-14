49b891fd2272e510ac549d264463cddd9067a7ff
Fengyuan Chen <cfy.org@gmail.com>
2015-08-14 11:57:50 +0800

    Improve search bar

--- admin/views/assets/javascripts/app/app.js
+++ admin/views/assets/javascripts/app/app.js
@@ -2,24 +2,30 @@ $(function () {
 
   'use strict';
 
+  var location = window.location;
+  var search = location.search;
+
   $('.qor-lang-selector, .qor-locale-selector').on('change', function () {
     var url = $(this).val();
 
     if (url) {
-      window.location.assign(url);
+      location.assign(url);
     }
   });
 
   $('.qor-search').each(function () {
     var $this = $(this);
-    var $input = $this.find('.qor-search-input');
-    var $clear = $this.find('.qor-search-clear');
+    var $input = $this.find('.qor-search__input');
+    var $clear = $this.find('.qor-search__clear');
+    var isSearched = !!$input.val();
 
-    $clear.on('click', function () {
-      $this.removeClass('is-dirty');
-
+    $clear.on('click', function (e) {
       if ($input.val()) {
         $input.focus().val('');
+      } else if (isSearched) {
+        location.search = search.replace(new RegExp($input.attr('name') + '\\=?\\w*'), '');
+      } else {
+        $this.removeClass('is-dirty');
       }
     });
   });
