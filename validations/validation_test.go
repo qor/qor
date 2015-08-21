bd662668913fa0a751fd8966899973af73ca4db9
Joseph Buchma <josephbuchma@gmail.com>
2015-08-21 21:27:44 +0300

    IntRange, FloatRange, LengthRange, Present, Unique

--- validations/validation_test.go
+++ validations/validation_test.go
@@ -1,155 +1,198 @@
 package validations_test
 
 import (
-	"errors"
-	"regexp"
+	"database/sql"
 	"testing"
 
+	"github.com/JosephBuchma/qor/test/utils"
+	"github.com/JosephBuchma/qor/validations"
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/qor/qor/test/utils"
-	"github.com/qor/qor/validations"
 )
 
 var db *gorm.DB
 
-type User struct {
-	gorm.Model
-	Name       string
-	CompanyID  int
-	Company    Company
-	CreditCard CreditCard
-	Addresses  []Address
-	Languages  []Language `gorm:"many2many:user_languages"`
-}
-
-func (user *User) Validate(db *gorm.DB) {
-	if user.Name == "invalid" {
-		db.AddError(validations.NewError(user, "Name", "invalid user name"))
-	}
-}
-
-type Company struct {
-	gorm.Model
-	Name string
-}
-
-func (company *Company) Validate(db *gorm.DB) {
-	if company.Name == "invalid" {
-		db.AddError(errors.New("invalid company name"))
-	}
-}
-
-type CreditCard struct {
-	gorm.Model
-	UserID int
-	Number string
-}
-
-func (card *CreditCard) Validate(db *gorm.DB) {
-	if !regexp.MustCompile("^(\\d){13,16}$").MatchString(card.Number) {
-		db.AddError(validations.NewError(card, "Number", "invalid card number"))
-	}
-}
-
-type Address struct {
-	gorm.Model
-	UserID  int
-	Address string
-}
-
-func (address *Address) Validate(db *gorm.DB) {
-	if address.Address == "invalid" {
-		db.AddError(validations.NewError(address, "Address", "invalid address"))
-	}
-}
-
-type Language struct {
-	gorm.Model
-	Code string
-}
-
-func (language *Language) Validate(db *gorm.DB) error {
-	if language.Code == "invalid" {
-		return validations.NewError(language, "Code", "invalid language")
-	}
-	return nil
-}
-
 func init() {
 	db = utils.TestDB()
 	validations.RegisterCallbacks(db)
-	db.AutoMigrate(&User{}, &Company{}, &CreditCard{}, &Address{}, &Language{})
+	db.AutoMigrate(&IntRangeTestModel{}, &FloatRangeTestModel{}, &PresentTestModel{}, &UniqueTestModel{}, &MultipleValidationsTestModel{})
+	db.Exec("truncate int_range_test_models;")
+	db.Exec("truncate float_range_test_models;")
+	db.Exec("truncate present_test_models;")
+	db.Exec("truncate unique_test_models;")
+	db.Exec("truncate multiple_validations_test_models;")
 }
 
-func TestSaveInvalidUser(t *testing.T) {
-	user := User{Name: "invalid"}
+// RangeInt validator
 
-	if result := db.Save(&user); result.Error == nil {
-		t.Errorf("Should get error when save invalid user")
-	}
+type IntRangeTestModel struct {
+	gorm.Model
+	Normal   int64
+	Nullable sql.NullInt64
 }
 
-func TestSaveInvalidCompany(t *testing.T) {
-	user := User{
-		Name:    "valid",
-		Company: Company{Name: "invalid"},
-	}
-
-	if result := db.Save(&user); result.Error == nil {
-		t.Errorf("Should get error when save invalid company")
-	}
+func (rc *IntRangeTestModel) Validate(db *gorm.DB) {
+	validates := validations.NewValidator(db, rc)
+	validates("Normal",
+		validations.RangeInt(5, 50))
+	validates("Nullable",
+		validations.RangeInt(5, 50))
 }
 
-func TestSaveInvalidCreditCard(t *testing.T) {
-	user := User{
-		Name:       "valid",
-		Company:    Company{Name: "valid"},
-		CreditCard: CreditCard{Number: "invalid"},
+func TestRangeInt(t *testing.T) {
+	ti := IntRangeTestModel{Normal: 33, Nullable: sql.NullInt64{Int64: 42, Valid: true}}
+	if res := db.Save(&ti); res.Error != nil {
+		t.Error("Should not get error when save ti record")
 	}
 
-	if result := db.Save(&user); result.Error == nil {
-		t.Errorf("Should get error when save invalid credit card")
-	}
-}
-
-func TestSaveInvalidAddresses(t *testing.T) {
-	user := User{
-		Name:       "valid",
-		Company:    Company{Name: "valid"},
-		CreditCard: CreditCard{Number: "4111111111111111"},
-		Addresses:  []Address{{Address: "invalid"}},
+	valid_with_null := IntRangeTestModel{Normal: 33, Nullable: sql.NullInt64{Int64: 0, Valid: false}}
+	if res := db.Save(&valid_with_null); res.Error != nil {
+		t.Error("Should not get error when save ti record with null value")
 	}
 
-	if result := db.Save(&user); result.Error == nil {
-		t.Errorf("Should get error when save invalid addresses")
+	invalid := IntRangeTestModel{Normal: 1, Nullable: sql.NullInt64{Int64: 452, Valid: true}}
+	if res := db.Save(&invalid); res.Error == nil {
+		t.Error("Should get error when save invalid record with null value")
 	}
 }
 
-func TestSaveInvalidLanguage(t *testing.T) {
-	user := User{
-		Name:       "valid",
-		Company:    Company{Name: "valid"},
-		CreditCard: CreditCard{Number: "4111111111111111"},
-		Addresses:  []Address{{Address: "valid"}},
-		Languages:  []Language{{Code: "invalid"}},
+// RangeFloat validator
+
+type FloatRangeTestModel struct {
+	gorm.Model
+	Normal   float64
+	Nullable sql.NullFloat64
+}
+
+func (rc *FloatRangeTestModel) Validate(db *gorm.DB) {
+	validates := validations.NewValidator(db, rc)
+	validates("Normal",
+		validations.RangeFloat(5.0, 50.0))
+	validates("Nullable",
+		validations.RangeFloat(5.0, 50.0))
+}
+
+func TestRangeFloat(t *testing.T) {
+	ti := FloatRangeTestModel{Normal: 33.0, Nullable: sql.NullFloat64{Float64: 42.0, Valid: true}}
+	if res := db.Save(&ti); res.Error != nil {
+		t.Error("Should not get error when save ti record")
 	}
 
-	if result := db.Save(&user); result.Error == nil {
-		t.Errorf("Should get error when save invalid language")
+	valid_with_null := FloatRangeTestModel{Normal: 33.0, Nullable: sql.NullFloat64{Float64: 0.0, Valid: false}}
+	if res := db.Save(&valid_with_null); res.Error != nil {
+		t.Error("Should not get error when save valid record with null value")
+	}
+
+	invalid := FloatRangeTestModel{Normal: 1.3, Nullable: sql.NullFloat64{Float64: 452.0, Valid: true}}
+	if res := db.Save(&invalid); res.Error == nil {
+		t.Error("Should get error when save invalid record with null value")
 	}
 }
 
-func TestSaveAllValidData(t *testing.T) {
-	user := User{
-		Name:       "valid",
-		Company:    Company{Name: "valid"},
-		CreditCard: CreditCard{Number: "4111111111111111"},
-		Addresses:  []Address{{Address: "valid1"}, {Address: "valid2"}},
-		Languages:  []Language{{Code: "valid1"}, {Code: "valid2"}},
+// Present validator
+
+type PresentTestModel struct {
+	gorm.Model
+	Int    sql.NullInt64
+	String sql.NullString
+	Bool   sql.NullBool
+}
+
+func (rc *PresentTestModel) Validate(db *gorm.DB) {
+	validates := validations.NewValidator(db, rc)
+	validates("Int",
+		validations.Present())
+	validates("String",
+		validations.Present())
+	validates("Bool",
+		validations.Present())
+}
+
+func TestPresent(t *testing.T) {
+	empty := &PresentTestModel{}
+	if res := db.Save(empty); res.Error == nil {
+		t.Error("Should get error when save unitialized struct")
 	}
 
-	if result := db.Save(&user); result.Error != nil {
-		t.Errorf("Should get no error when save valid data, but got: %v", result.Error)
+	ti := &PresentTestModel{}
+	ti.Int.Scan(24)
+	ti.String.Scan("ti string")
+	ti.Bool.Scan(true)
+	if res := db.Save(ti); res.Error != nil {
+		t.Error("Should not get error when save ti struct")
+	}
+}
+
+// Unique validator
+
+type UniqueTestModel struct {
+	gorm.Model
+	UniqueInt    int
+	UniqueString sql.NullString
+}
+
+func (rc *UniqueTestModel) Validate(db *gorm.DB) {
+	validates := validations.NewValidator(db, rc)
+	validates("UniqueInt",
+		validations.Unique())
+	validates("UniqueString",
+		validations.Unique())
+}
+
+func TestUnique(t *testing.T) {
+	ti := &UniqueTestModel{}
+	ti.UniqueInt = 3
+	ti.UniqueString.Scan("unique string")
+	db.Save(ti)
+	ti.ID = 0
+	if err := db.Save(ti); err == nil {
+		t.Errorf("Should get error when save non-unique record")
+	}
+	ti.ID = 0
+	ti.UniqueInt = 4
+	ti.UniqueString.Scan("Another unique string")
+	if res := db.Save(ti); res.Error != nil {
+		t.Errorf("Should not get error when save unique record")
+	}
+	ti.ID = 0
+	ti.UniqueInt = 5
+	ti.UniqueString = sql.NullString{String: "", Valid: false}
+	if res := db.Save(ti); res.Error != nil {
+		t.Errorf("Should not get error when save unique record with null value")
+	}
+}
+
+// Multivalidations
+
+type MultipleValidationsTestModel struct {
+	gorm.Model
+	SomeString sql.NullString
+}
+
+func (rc *MultipleValidationsTestModel) Validate(db *gorm.DB) {
+	validates := validations.NewValidator(db, rc)
+	validates("SomeString",
+		validations.Present(),
+		validations.Unique(),
+		validations.LengthRange(8, 8))
+}
+
+func TestMultipleValidators(t *testing.T) {
+	ti := MultipleValidationsTestModel{}
+	if res := db.Save(&ti); res.Error == nil {
+		t.Errorf("Should get error when SomeString of record is NULL")
+	}
+	ti.SomeString.Scan("12345678")
+	if res := db.Save(&ti); res.Error != nil {
+		t.Errorf("Should not get error when SomeString of record is unique, not NULL, has 8 characters")
+	}
+	ti.ID = 0
+	if res := db.Save(&ti); res.Error == nil {
+		t.Errorf("Should get error when SomeString of record is not NULL, has 8 characters, but not unique")
+	}
+	ti.ID = 0
+	ti.SomeString.Scan("123")
+	if res := db.Save(&ti); res.Error == nil {
+		t.Errorf("Should get error when SomeString of record is unique, not NULL, but hasn't 8 characters")
 	}
 }
