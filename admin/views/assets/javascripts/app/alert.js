1c3b20057bee7c680a738dcef7e25212550101cc
Fengyuan Chen <cfy.org@gmail.com>
2015-08-25 12:09:22 +0800

    Simplify alert code

--- admin/views/assets/javascripts/app/alert.js
+++ admin/views/assets/javascripts/app/alert.js
@@ -2,14 +2,12 @@ $(function () {
 
   'use strict';
 
-  $(function () {
-    $(document).on('click.qor.alert', '[data-dismiss="alert"]', function () {
-      $(this).closest('.qor-alert').remove();
-    });
-
-    setTimeout(function () {
-      $('.qor-alert[data-dismissible="true"]').remove();
-    }, 3000);
+  $(document).on('click.qor.alert', '[data-dismiss="alert"]', function () {
+    $(this).closest('.qor-alert').remove();
   });
+
+  setTimeout(function () {
+    $('.qor-alert[data-dismissible="true"]').remove();
+  }, 5000);
 
 });
