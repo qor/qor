7be794df09ae51eadd097a9af8089aec8887135c
Jinzhu <wosmvp@gmail.com>
2015-10-14 18:20:23 +0800

    Add vips handler for image

---
+++ media_library/handlers/vips/vips.go
@@ -0,0 +1,84 @@
+package vips
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+
+	"github.com/qor/qor/media_library"
+
+	"gopkg.in/h2non/bimg.v0"
+)
+
+type bimgImageHandler struct{}
+
+func (bimgImageHandler) CouldHandle(media media_library.MediaLibrary) bool {
+	return media.IsImage()
+}
+
+func (bimgImageHandler) Handle(media media_library.MediaLibrary, file multipart.File, option *media_library.Option) error {
+	// Save Original Image
+	if err := media.Store(media.URL("original"), option, file); err == nil {
+		file.Seek(0, 0)
+
+		// Crop & Resize
+		var buffer bytes.Buffer
+		if _, err := io.Copy(&buffer, file); err != nil {
+			return err
+		}
+
+		img := bimg.NewImage(buffer.Bytes())
+
+		// Handle original image
+		{
+			bimgOption := bimg.Options{Interlace: true}
+
+			// Crop original image if specified
+			if cropOption := media.GetCropOption("original"); cropOption != nil {
+				bimgOption.Top = cropOption.Min.Y
+				bimgOption.Left = cropOption.Min.X
+				bimgOption.AreaWidth = cropOption.Max.X - cropOption.Min.X
+				bimgOption.AreaHeight = cropOption.Max.Y - cropOption.Min.Y
+			}
+
+			// Process & Save original image
+			if buf, err := img.Process(bimgOption); err == nil {
+				media.Store(media.URL(), option, bytes.NewReader(buf))
+			} else {
+				return err
+			}
+		}
+
+		// Handle size images
+		for key, size := range media.GetSizes() {
+			bimgOption := bimg.Options{
+				Interlace: true,
+				Enlarge:   true,
+				Width:     size.Width,
+				Height:    size.Height,
+			}
+
+			if cropOption := media.GetCropOption(key); cropOption != nil {
+				bimgOption.Top = cropOption.Min.Y
+				bimgOption.Left = cropOption.Min.X
+				bimgOption.AreaWidth = cropOption.Max.X - cropOption.Min.X
+				bimgOption.AreaHeight = cropOption.Max.Y - cropOption.Min.Y
+				bimgOption.Crop = true
+			}
+
+			// Process & Save size image
+			if buf, err := img.Process(bimgOption); err == nil {
+				media.Store(media.URL(key), option, bytes.NewReader(buf))
+			} else {
+				return err
+			}
+		}
+		return nil
+	} else {
+		return err
+	}
+}
+
+func init() {
+	media_library.RegisterMediaLibraryHandler("image_handler", bimgImageHandler{})
+}
