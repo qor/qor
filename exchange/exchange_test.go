26ab19e502d820717a418d2e05f2b94f65b7549e
Jinzhu <wosmvp@gmail.com>
2015-10-21 15:24:43 +0800

    Make exchange test fail

--- exchange/exchange_test.go
+++ exchange/exchange_test.go
@@ -14,9 +14,21 @@ var product *exchange.Resource
 
 func init() {
 	db = utils.TestDB()
+
+	db.DropTable(&Product{})
+	db.AutoMigrate(&Product{})
+
 	product = exchange.NewResource(&Product{})
 }
 
 func TestImportCSV(t *testing.T) {
-	product.Import(csv.New("fixtures/products.csv"), nil)
+	if err := product.Import(csv.New("fixtures/products.csv"), nil); err != nil {
+		t.Fatalf("Failed to import csv, get error %v", err)
+	}
+
+	var products []Product
+	db.Find(&products)
+	if len(products) != 3 {
+		t.Errorf("Failed to find importted products, got %v", len(products))
+	}
 }
