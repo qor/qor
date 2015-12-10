07c41c405e147dc16da4eff59b408e3dcc65f3a9
Jinzhu <wosmvp@gmail.com>
2015-12-10 14:23:07 +0800

    Refactor responder

--- responder/responder.go
+++ responder/responder.go
@@ -6,45 +6,66 @@ import (
 	"strings"
 )
 
+var MimeTypes = map[string]string{}
+
+func Register(mime string, format string) {
+	MimeTypes[mime] = format
+}
+
+func init() {
+	for mimeType, format := range map[string]string{
+		"text/html":        "html",
+		"application/json": "json",
+	} {
+		Register(mimeType, format)
+	}
+}
+
 type Responder struct {
 	responds map[string]func()
 }
 
-func With(format string, fc func()) *Responder {
+// With support string or []string as formats, With("html", func() {
+//   writer.Write([]byte("html"))
+// }).With([]string{"json", "xml"}, func() {
+//   writer.Write([]byte("json or xml"))
+// })
+func With(format interface{}, fc func()) *Responder {
 	rep := &Responder{responds: map[string]func(){}}
 	return rep.With(format, fc)
 }
 
-func (rep *Responder) With(format string, fc func()) *Responder {
-	rep.responds[format] = fc
+func (rep *Responder) With(format interface{}, fc func()) *Responder {
+	if f, ok := format.(string); ok {
+		rep.responds[f] = fc
+	} else if fs, ok := format.([]string); ok {
+		for _, f := range fs {
+			rep.responds[f] = fc
+		}
+	}
 	return rep
 }
 
 func (rep *Responder) Respond(writer http.ResponseWriter, request *http.Request) {
-	format := "html"
+	// get request format from url
 	if ext := filepath.Ext(request.URL.Path); ext != "" {
-		format = strings.TrimPrefix(ext, ".")
+		if respond, ok := rep.responds[strings.TrimPrefix(ext, ".")]; ok {
+			respond()
+			return
+		}
 	}
 
-	if request.Header.Get("Content-Type") == "application/json" {
-		format = "json"
-	} else {
-		for _, str := range strings.Split(request.Header.Get("Accept"), ",") {
-			if str == "application/json" {
-				format = "json"
-				if writer != nil {
-					writer.Header().Set("Content-Type", "application/json")
-				}
-				break
+	// get request format from Accept
+	for _, accept := range strings.Split(request.Header.Get("Accept"), ",") {
+		if format, ok := MimeTypes[accept]; ok {
+			if respond, ok := rep.responds[format]; ok {
+				respond()
+				return
 			}
 		}
 	}
 
-	if fc, ok := rep.responds[format]; ok {
-		fc()
-		return
-	}
-
+	// use first format as default
 	for _, respond := range rep.responds {
 		respond()
 		break
