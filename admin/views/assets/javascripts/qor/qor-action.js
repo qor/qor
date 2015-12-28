0ea623b9839347bc5e877dca05e4e879edbf4aa9
binku <binku87@gmail.com>
2015-12-28 15:44:09 +0800

    action: format js

--- admin/views/assets/javascripts/qor/qor-action.js
+++ admin/views/assets/javascripts/qor/qor-action.js
@@ -33,15 +33,13 @@
     constructor: QorAction,
 
     init: function () {
-      var options = this.options;
-      var $this = this.$element;
       this.bind();
     },
 
     bind: function () {
       this.$element.on(EVENT_CLICK, $.proxy(this.check, this));
       this.$element.on(EVENT_CHNAGE, $.proxy(this.change, this));
-      this.$wrap.on(EVENT_SUBMIT, "form", $.proxy(this.submit, this));
+      this.$wrap.on(EVENT_SUBMIT, 'form', $.proxy(this.submit, this));
     },
 
     unbind: function () {
@@ -50,21 +48,21 @@
       this.$element.off(EVENT_SUBMIT, this.submit);
     },
 
-    change : function(e) {
+    change : function (e) {
       var $target = $(e.target);
 
       if ($target.is('.qor-js-selector')) {
-        $(".qor-action-wrap .qor-js-form").hide();
-        $(".qor-action-wrap .qor-js-form[data-action='" + $target.val() + "']").show();
+        $('.qor-action-wrap .qor-js-form').hide();
+        $('.qor-action-wrap .qor-js-form[data-action="' + $target.val() + '"]').show();
         $.proxy(this.appendCheckbox, $target)();
       }
     },
 
-    submit : function(e) {
+    submit : function (e) {
       var $form = $(e.target);
       $.proxy(this.appendCheckInputs, $form)();
-      var $submit = $form.find("button");
-      $form.find("qor-js-loading").show();
+      var $submit = $form.find('button');
+      $form.find('qor-js-loading').show();
       $.ajax($form.prop('action'), {
         method: $form.prop('method'),
         data: new FormData($form.get(0)),
@@ -117,42 +115,42 @@
     },
 
     // Helper
-    appendCheckbox : function() {
+    appendCheckbox : function () {
       // Only value change and the table isn't selectable will add checkboxes
-      $(".qor-page__body .mdl-data-table__select").each(function(i, e) { $(e).parents("td").remove() });
-      $(".qor-page__body .mdl-data-table__select").each(function(i, e) { $(e).parents("th").remove() });
+      $('.qor-page__body .mdl-data-table__select').each(function (i, e) { $(e).parents('td').remove(); });
+      $('.qor-page__body .mdl-data-table__select').each(function (i, e) { $(e).parents('th').remove(); });
 
-      if($(this).val()) {
-        $(".qor-page__body table").addClass("mdl-data-table--selectable");
-        new window.MaterialDataTable($(".qor-page__body table").get(0));
+      if ($(this).val()) {
+        $('.qor-page__body table').addClass('mdl-data-table--selectable');
+        window.newQorMaterialDataTable = new window.MaterialDataTable($('.qor-page__body table').get(0));
 
         // The fixed head have checkbox but the visiual one doesn't, clone the head with checkbox from the fixed one
-        $("thead.is-hidden tr th:not('.mdl-data-table__cell--non-numeric')").clone().prependTo($("thead:not('.is-hidden') tr"));
+        $('thead.is-hidden tr th:not(".mdl-data-table__cell--non-numeric")').clone().prependTo($('thead:not(".is-hidden") tr'));
 
         // The clone one doesn't bind event, so binding event manual
-        var $fixedHeadCheckBox = $("thead:not('.is-fixed') .mdl-checkbox__input").parents("label");
-        $fixedHeadCheckBox.find("span").remove();
-        new MaterialCheckbox($fixedHeadCheckBox.get(0));
-        $fixedHeadCheckBox.click(function(e) {
-          $("thead.is-fixed tr th").eq(0).find("label").click();
-          $(this).toggleClass("is-checked");
+        var $fixedHeadCheckBox = $('thead:not(".is-fixed") .mdl-checkbox__input').parents('label');
+        $fixedHeadCheckBox.find('span').remove();
+        window.newQorMaterialCheckbox = new window.MaterialCheckbox($fixedHeadCheckBox.get(0));
+        $fixedHeadCheckBox.click(function () {
+          $('thead.is-fixed tr th').eq(0).find('label').click();
+          $(this).toggleClass('is-checked');
           return false;
         });
       } else {
-        $(".qor-page__body table.mdl-data-table--selectable").removeClass("mdl-data-table--selectable");
-        $(".qor-page__body tr.is-selected").removeClass("is-selected");
+        $('.qor-page__body table.mdl-data-table--selectable').removeClass('mdl-data-table--selectable');
+        $('.qor-page__body tr.is-selected').removeClass('is-selected');
       }
     },
 
-    appendCheckInputs: function() {
+    appendCheckInputs: function () {
       var $form = $(this);
-      $form.find("input[type='hidden']").remove();
-      $(".qor-page__body .mdl-checkbox__input:checked").each(function(i, e) {
-        var id = $(e).parents("tr").data("primary-key");
+      $form.find('input[type="hidden"]').remove();
+      $('.qor-page__body .mdl-checkbox__input:checked').each(function (i, e) {
+        var id = $(e).parents('tr').data('primary-key');
         $form.prepend('<input type="hidden" name="IDs[]" value="' + id + '" />');
       });
-    },
-  },
+    }
+  };
 
   QorAction.DEFAULTS = {
   };
