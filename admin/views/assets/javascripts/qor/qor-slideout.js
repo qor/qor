f6e61ff366b0cb596232ad2c83f287fce48d7ac3
jason weng <wengjianm@gmail.com>
2016-01-19 14:54:07 +0800

    refactor(datetime picker): refactor datetime picker, suppoer time picker

--- admin/views/assets/javascripts/qor/qor-slideout.js
+++ admin/views/assets/javascripts/qor/qor-slideout.js
@@ -272,6 +272,9 @@ $.fn.qorSliderAfterShow = {};
 
               this.show();
 
+              $('.qor-datetime-picker').materialDatePicker({ format : 'YYYY-MM-DD HH:mm' });
+              $('.qor-date-picker').materialDatePicker({ format : 'YYYY-MM-DD', time: false });
+
               // callback for after slider loaded HTML
               if (options.afterShow){
                 var qorSliderAfterShow = $.fn.qorSliderAfterShow;
