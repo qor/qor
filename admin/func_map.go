b045d9d0f880a5bd58c22c21f5823d76c2eb0f68
Jinzhu <wosmvp@gmail.com>
2016-02-16 17:58:11 +0800

    Support scanner & valuer

--- admin/func_map.go
+++ admin/func_map.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"bytes"
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"html"
@@ -117,6 +118,14 @@ func (context *Context) valueOf(valuer func(interface{}, *qor.Context) interface
 					return nil
 				}
 			}
+
+			// For scanner & valuer
+			if value, ok := result.(driver.Valuer); ok {
+				if value, err := value.Value(); err == nil {
+					return value
+				}
+			}
+
 			return result
 		} else {
 			return nil
