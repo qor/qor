5538803447e007ae5b85c3cd229f665fbe15df0f
Jinzhu <wosmvp@gmail.com>
2016-02-29 17:20:32 +0800

    Refactor qor package based on golint suggestions

--- context.go
+++ context.go
@@ -6,10 +6,12 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// CurrentUser is an interface, which is used for qor admin to get current logged user
 type CurrentUser interface {
 	DisplayName() string
 }
 
+// Context qor context, which is used for many qor components, used to share information between them
 type Context struct {
 	Request     *http.Request
 	Writer      http.ResponseWriter
@@ -21,19 +23,21 @@ type Context struct {
 	Errors
 }
 
+// Clone clone current context
 func (context *Context) Clone() *Context {
 	var clone = *context
 	return &clone
 }
 
+// GetDB get db from current context
 func (context *Context) GetDB() *gorm.DB {
 	if context.DB != nil {
 		return context.DB
-	} else {
-		return context.Config.DB
 	}
+	return context.Config.DB
 }
 
+// SetDB set db into current context
 func (context *Context) SetDB(db *gorm.DB) {
 	context.DB = db
 }
