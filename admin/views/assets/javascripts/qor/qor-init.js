6541a15dc3a4ac6d02d4c2c88cb74af97693c1f8
Jinzhu <wosmvp@gmail.com>
2016-03-08 14:03:15 +0800

    Remove admin, publish from qor repo

--- admin/views/assets/javascripts/qor/qor-init.js
+++
@@ -1,9 +0,0 @@
-// init for slideout after show event
-$.fn.qorSliderAfterShow = {};
-
-// change Mustache tags from {{}} to [[]]
-window.Mustache.tags = ['[[', ']]'];
-
-// Init for date time picker
-$('[data-toggle="qor.datetimepicker"]').materialDatePicker({ format : 'YYYY-MM-DD HH:mm' });
-$('[data-toggle="qor.datepicker"]').materialDatePicker({ format : 'YYYY-MM-DD', time: false });
